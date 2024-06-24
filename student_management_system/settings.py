59ef97df490effb55b8e2cdbea636c32a884e110
hassanyange <hassanyange@gmail.com>
2024-06-24 06:15:14 -0700

    some changes

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'  # Add a leading slash
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
